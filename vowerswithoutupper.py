9430dbf338ed565d9f0801192def07b9539190f0
psneha-pillai <pillaisneha23@gmail.com>
2025-05-14 20:48:29 +0530

    Add files via upload

---
+++ vowerswithoutupper.py
@@ -0,0 +1,11 @@
+n=input("enter")
+result=""
+for char in n:
+    if char in "aeiou":
+        result+= char(ord(char)-32)
+    elif char in n:
+        result+=char(ord(char)+32)
+    else:
+        result+=char
+print(result)
+
