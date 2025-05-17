a10c79179186d744a9c5cf08bad8bf79e8281479
psneha-pillai <pillaisneha23@gmail.com>
2025-05-17 11:49:02 +0530

    Add files via upload

---
+++ pow(x,n).py
@@ -0,0 +1,20 @@
+'''def power(x, n):
+    return x ** n
+x=int(input())
+n=int(input())
+print(power(x,n))'''
+
+'''if values are negative
+def mypow(n,x):
+    if(n<0):
+        x=1/x
+        n=abs(n)
+        ans=1
+        for i in range(n):
+            ans=ans*x
+        return ans
+n=int(input())
+x=int(input())
+print(mypow(n,x))'''
+
+using recurtion
