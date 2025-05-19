c443b0b27239fcbe2f4148b1a14755adbddd7933
psneha-pillai <pillaisneha23@gmail.com>
2025-05-19 14:57:42 +0530

    Add files via upload

---
+++ inserctionSort.py
@@ -0,0 +1,9 @@
+def insertionSort(arr):
+    n=len(arr)
+    for i in range(0,n):
+        while i>0 and arr[i-1]>arr[i]:
+            arr[i-1],arr[i]=arr[i],arr[i-1]
+            i-=1
+    return arr
+arr=list(map(int,input().split()))
+print(insertionSort(arr))
