c4ddf9a8e0565ebf6132cd0f41b2d000997f42e8
psneha-pillai <pillaisneha23@gmail.com>
2025-05-19 14:58:08 +0530

    Add files via upload

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(arr):
+    for i in range(n-1,-1,-1):
+        for j in range(i):
+            if(arr[j]>arr[j+1]):
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr=list(map(int,input().split()))
+n=len(arr)
+print(bubbleSort(arr))
